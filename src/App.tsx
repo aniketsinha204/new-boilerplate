8b53a5677c4464b3e2073588d5acea892c558dab
Aniket Sinha <aniket@Anikets-MacBook-Pro.local>
2022-02-07 14:17:44 +0530

    added husky and removed test commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const hello = 'sdgfs';
     return (
         <div className="App">
             <header className="App-header">
